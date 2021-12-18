e27f5badd68b9ccc185fdef5415944a6f2bb4b9c
Aybars Acar <aybars.dev@gmail.com>
2021-12-19 08:45:02 +1100

    Initial Commit

---
+++ AuthDemo/AuthDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthDemoApp.swift
+//  AuthDemo
+//
+//  Created by Aybars Acar on 19/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct AuthDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
